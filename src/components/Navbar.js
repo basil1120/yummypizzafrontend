9221ea83354a0d77d521929d155af78492461ba4
Jeremiah Aura [Digital Banking] <jaura@CO-OPBANK.CO.KE>
2020-05-01 17:35:04 +0300

    few file updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
     return(
             <nav className="nav-wrapper">
                 <div className="container">
-                    <Link to="/" className="brand-logo">Yummy Pizza e-Shop</Link>
+                    <Link to="/" className="brand-logo">YUMMY PIZZA e-Shop</Link>
                     
                     <ul className="right">
                         <li><Link to="/">Shop</Link></li>
-                        <li><Link to="#">History</Link></li>
+                        <li><Link to="/">History</Link></li>
                         <li><Link to="/cart">My cart</Link></li>
                         <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                     </ul>
